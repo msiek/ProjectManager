89a145e0816f4afd9e5b2413d64c4007d6139fb3
msiek <michael_siek123@hotmail.con>
2017-10-26 09:48:09 -0400

    React JS Project Manager App

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: []
+        }
+    }
+
+    componentWillMount(){
+        this.setState({projects: [
+            {
+                title: 'Business Website',
+                category: 'Web Design'
+            },
+            {
+                title: 'Social App',
+                category: 'Mobile Development'
+            },
+            {
+                title: 'E-Commerce Shopping Cart',
+                category: 'Web Development'
+            },
+
+        ]});
+
+    }
+
+  render() {
+    return (
+      <div className="App">
+        <AddProject/>
+          <Projects projects={this.state.projects}/>
+      </div>
+    );
+  }
+}
+
+export default App;
